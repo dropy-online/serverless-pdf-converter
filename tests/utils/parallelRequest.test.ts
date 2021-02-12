f9a68c3436acc28c31d12e20046e0440833e990f
aereeeee <dofl5576@gmail.com>
2021-02-12 23:12:04 +0900

    Fix broken tests

--- tests/utils/parallelRequest.test.ts
+++ tests/utils/parallelRequest.test.ts
@@ -1,25 +1,27 @@
 import { parallelRequest } from '@/utils';
-import aws from 'aws-sdk';
+import { Lambda as LambdaMock } from 'aws-sdk';
 
-jest.mock('aws-sdk');
+jest.mock('aws-sdk', () => {
+  const mock = { invoke: jest.fn() };
+  return { Lambda: jest.fn(() => mock) };
+});
 
 describe('parallelRequest()', () => {
   const FunctionName = 'functionName';
   const items = [1, 2, 3];
   const params = { option: 'option' };
+  const Lambda = new LambdaMock();
 
   it('should invoke lambda and return resolved data array', async () => {
     expect.hasAssertions();
 
-    const invoke = jest.fn().mockImplementation((payload, cb) => {
+    (Lambda.invoke as jest.Mocked<any>).mockImplementation((payload, cb) => {
       cb(null, { Payload: payload });
     });
-    jest.spyOn(aws, 'Lambda').mockImplementation(() => ({ invoke }));
 
     const result = await parallelRequest(FunctionName, items, params);
 
     const expected = items.map((item) => ({
-      InvocationType: 'Event',
       FunctionName,
       Payload: JSON.stringify({ item, params }),
     }));
@@ -30,10 +32,9 @@ describe('parallelRequest()', () => {
     expect.hasAssertions();
 
     const error = 'error';
-    const invoke = jest.fn().mockImplementation((_, cb) => {
+    (Lambda.invoke as jest.Mocked<any>).mockImplementation((_, cb) => {
       cb(new Error(error));
     });
-    jest.spyOn(aws, 'Lambda').mockImplementation(() => ({ invoke }));
 
     await expect(parallelRequest(FunctionName, items, params)).rejects.toThrow(error);
   });
