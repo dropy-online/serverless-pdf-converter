bfaff67ae93cf639b9ca1b05113567e25cb591c2
aereeeee <dofl5576@gmail.com>
2021-02-12 23:09:14 +0900

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,41 +4,39 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'prettier',
     'airbnb-base',
     'plugin:jest/all',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint', 'jest',
-  ],
+  plugins: ['@typescript-eslint', 'jest'],
   settings: {
     'import/resolver': {
       alias: {
-        map: [
-          ['@', './src'],
-        ],
-        extensions: [
-          '.ts',
-          '.js',
-        ],
+        map: [['@', './src']],
+        extensions: ['.ts', '.js'],
       },
     },
   },
   rules: {
-    'max-len': ['error', { code: 120 }],
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'implicit-arrow-linebreak': 'off',
     'import/prefer-default-export': 'off',
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
+    'jest/no-commented-out-tests': 'off',
+    'comma-dangle': 'off',
+    'no-confusing-arrow': 'off',
   },
 };
