0b0aa66d4861a3ae421e7e790ccf2eb64163d88d
aereeeee <dofl5576@gmail.com>
2021-02-23 22:24:44 +0900

    Add ts-jest for test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   setupFiles: ['./tests/setup/setEnvironment.js'],
-  transform: {
-    '^.+\\.ts?$': 'babel-jest',
-  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'node',
+  preset: 'ts-jest',
 };
